b4dc14684f8cf3eb292ac1b7a24da03c1b4eaa27
The Bearodactyl <dabewodactowluwu@gmail.com>
2025-08-31 19:02:16 -0500

    add admin status checker

--- src/handlers/misc.rs
+++ src/handlers/misc.rs
@@ -3,7 +3,7 @@ use rocket::{get, http::Status, routes as rocket_routes, serde::json::Json};
 use rocket_db_pools::Connection;
 use serde::{Deserialize, Serialize};
 
-use crate::db::BearoData;
+use crate::{auth::User, db::BearoData};
 
 #[derive(Serialize, Deserialize, Clone, Default)]
 pub struct CollectionStatus {
@@ -14,6 +14,15 @@ pub struct CollectionStatus {
 pub struct HealthStatus {
     pub db_status: String,
     pub collections_status: CollectionStatus,
+}
+
+#[derive(Serialize)]
+pub struct CheckStatusResponse {
+    valid: bool,
+    is_admin: bool,
+    user_id: String,
+    created_at: String,
+    last_used_at: Option<String>,
 }
 
 #[get("/check-health")]
@@ -41,7 +50,6 @@ pub async fn health(db: Connection<BearoData>) -> Result<Json<HealthStatus>, Sta
                 collections_status.books = "Books collection online!".to_string();
             }
         });
-
     } else {
         health.db_status = "database offline :(".to_string();
     }
@@ -49,6 +57,19 @@ pub async fn health(db: Connection<BearoData>) -> Result<Json<HealthStatus>, Sta
     Ok(Json(health))
 }
 
+#[get("/check-login")]
+pub async fn check_admin_status(user: User) -> Json<CheckStatusResponse> {
+    Json(CheckStatusResponse {
+        valid: true,
+        is_admin: user.is_admin(),
+        user_id: user.id().to_string(),
+        created_at: user.created_at().format("%Y-%m-%d %H:%M:%S").to_string(),
+        last_used_at: user
+            .last_used_at()
+            .map(|dt| dt.format("%Y-%m-%d %H:%M:%S").to_string()),
+    })
+}
+
 pub fn routes() -> Vec<rocket::Route> {
-    rocket_routes![health]
+    rocket_routes![health, check_admin_status]
 }
